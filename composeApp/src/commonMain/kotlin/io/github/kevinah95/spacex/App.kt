70e13127f26d2c300555fb2d83aa3090eaf8f513
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2025-11-04 22:41:42 -0600

    chore: :bookmark: prerelease 1.2.4-alpha

--- composeApp/src/commonMain/kotlin/io/github/kevinah95/spacex/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/kevinah95/spacex/App.kt
@@ -48,7 +48,7 @@ fun App() {
     val coroutineScope = rememberCoroutineScope()
     var isRefreshing by remember { mutableStateOf(false) }
     val pullToRefreshState = rememberPullToRefreshState()
-    val versionName = "1.2.3" // x-release-please-version
+    val versionName = "1.2.4-alpha" // x-release-please-version
 
     AppTheme {
         Scaffold(
