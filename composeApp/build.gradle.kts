6de45402755da9b7c9f9d58b39b21f8aadc210b3
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2025-11-06 12:35:01 -0600

    chore: :bookmark: prerelease 1.3.0-alpha

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -78,7 +78,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         // Keep versionName managed by release-please; versionCode is derived from it.
-        val relVersionName = "1.2.6" // x-release-please-version
+        val relVersionName = "1.3.0-alpha" // x-release-please-version
         versionName = relVersionName
         versionCode = versionCodeFrom(relVersionName)
     }
