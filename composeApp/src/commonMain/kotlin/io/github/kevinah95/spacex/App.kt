fee5e1d94a3c90ad675c1b92a7bd7d3ebae843a7
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2025-11-09 18:47:29 -0600

    ci: update version name for release process
    Updated the version name in App.kt to reflect the current release version.
    This change ensures that the application displays the correct version during
    the release process and aligns with the semantic release strategy.

--- composeApp/src/commonMain/kotlin/io/github/kevinah95/spacex/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/kevinah95/spacex/App.kt
@@ -48,7 +48,7 @@ fun App() {
     val coroutineScope = rememberCoroutineScope()
     var isRefreshing by remember { mutableStateOf(false) }
     val pullToRefreshState = rememberPullToRefreshState()
-    val versionName = "1.3.0-alpha.1" // x-release-please-version
+    val versionName = "1.3.0-alpha.1"
 
     AppTheme {
         Scaffold(
