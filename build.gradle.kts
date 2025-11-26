fbbca4e653084322f1809b0e08ea674072ecd386
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2025-11-26 16:29:18 -0600

    fix(build): add buildSrc settings for project configuration
    This commit introduces a new settings.gradle.kts file in the
    buildSrc directory, setting the root project name to "buildSrc".
    This change is necessary for organizing build logic and
    dependencies in a modular way, improving maintainability. Also to remove expect-actual and buildSrc warnings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,11 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
+
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.add("-Xexpect-actual-classes")
+        }
+    }
+}
