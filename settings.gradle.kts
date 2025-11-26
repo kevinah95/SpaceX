1ace9a3cf1b511f4233293c9a21b94b70a176541
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2025-11-26 16:11:21 -0600

    feat(develocity): add develocity plugin configuration
    Add configuration for the develocity plugin to enable build scans.
    This change allows for better insights into build performance and
    issues, enhancing the overall development experience.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,5 +28,18 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.develocity") version("4.2.2")
+}
+
+// Ref: https://docs.gradle.com/develocity/gradle/current/gradle-plugin/
+develocity {
+    buildScan {
+        publishing.onlyIf { !System.getenv("CI").isNullOrEmpty() }
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
+
 include(":composeApp")
 include(":shared")
