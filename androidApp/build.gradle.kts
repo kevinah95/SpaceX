79def0d63adad3dea502e726fb70d4bbc957aef0
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2026-02-16 22:57:30 -0600

    chore(build): remove unused Koin compose dependencies
    Removed Koin compose dependencies from the build.gradle.kts file
    to clean up the project and reduce unnecessary bloat. These
    dependencies are not being utilized in the current implementation.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -33,9 +33,6 @@ kotlin {
     implementation(libs.kotlinx.coroutines.android)
     implementation(project.dependencies.platform(libs.koin.bom))
     implementation(libs.koin.android)
-    implementation(libs.koin.compose)
-    implementation(libs.koin.compose.viewmodel)
-    implementation(libs.koin.compose.viewmodel.navigation)
   }
 }
 
