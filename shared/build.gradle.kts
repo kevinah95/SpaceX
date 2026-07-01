33787bfe7b0e5a16568f6a96045d050e6f1ede27
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2026-06-30 23:35:09 -0600

    build: update dependency versions and add Koin comment in shared build configuration

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -69,6 +69,7 @@ kotlin {
     commonTest.dependencies {
       implementation(libs.kotlin.test)
       implementation(libs.kotlinx.coroutines.test)
+      // Koin
       implementation(libs.koin.test)
       implementation(libs.ktor.client.mock)
     }
